d966d751b6c0ea8034b2bc29bad9963bcd7f55e3
Alexander Cho <alexanderswcho@gmail.com>
2026-03-08 22:25:49 -0700

    cleanup

--- client/contexts/GoogleAuth.js
+++ client/contexts/GoogleAuth.js
@@ -1,23 +1,23 @@
-import * as WebBrowser from 'expo-web-browser';
-import * as Google from 'expo-auth-session/providers/google';
+import { maybeCompleteAuthSession } from 'expo-web-browser';
+import { useAuthRequest } from 'expo-auth-session/providers/google';
 import { useEffect } from 'react';
 
-WebBrowser.maybeCompleteAuthSession();
+maybeCompleteAuthSession();
 
 export function useGoogleAuth(onSuccess) {
-    const [request, response, promptAsync] = Google.useAuthRequest({
-        webClientId: '347589405886-e0af02uaf6j4p15f0q1aimp7b577mjfo.apps.googleusercontent.com',
-        expoClientId: 'https://auth.expo.io/@alexanderswcho/prompt-chat',
+    const [request, response, promptAsync] = useAuthRequest({
+        webClientId: process.env.EXPO_PUBLIC_WEB_CLIENT_ID,
+        expoClientId: process.env.EXPO_PUBLIC_EXPO_CLIENT_ID,
         responseType: 'id_token',
         scopes: ['openid', 'profile', 'email']
     });
 
     useEffect(() => {
-        if (response?.type === 'success') {
-            if (onSuccess) onSuccess({
+        if (response?.type === 'success' && onSuccess) {
+            onSuccess({
                 idToken: response.params.id_token,
                 accessToken: response.authentication?.accessToken
-            });;
+            });
         }
     }, [response, onSuccess]);
 
